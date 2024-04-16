2bd4ff5e5c1febdd59b814d0bf3ba269aa3f25f5
Thomas <viltetomas2003@gmail.com>
2024-04-16 02:15:44 -0300

    feat: Agregar función para reproducir audio en canal de voz

--- go.mod
+++ go.mod
@@ -3,11 +3,10 @@ module github.com/Tomas-vilte/GoMusicBot
 go 1.21.2
 
 require (
-	github.com/bitly/go-simplejson v0.5.1 // indirect
-	github.com/dlclark/regexp2 v1.11.0 // indirect
-	github.com/dop251/goja v0.0.0-20240220182346-e401ed450204 // indirect
-	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
-	github.com/google/pprof v0.0.0-20240227163752-401108e1b7e7 // indirect
-	github.com/kkdai/youtube/v2 v2.10.1 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	github.com/bwmarrin/dgvoice v0.0.0-20210225172318-caaac756e02e // indirect
+	github.com/bwmarrin/discordgo v0.28.1 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+	layeh.com/gopus v0.0.0-20210501142526-1ee02d434e32 // indirect
 )
--- internal/spotify/auth.go
+++ internal/spotify/auth.go
@@ -1,38 +1,50 @@
-package auth
+package spotify
 
 import (
-	"bytes"
 	"encoding/base64"
 	"encoding/json"
+	"log"
 	"net/http"
 	url2 "net/url"
+	"strings"
 	"time"
 )
 
-type SpotifyAuth struct {
-	ClientID     string
-	ClientSecret string
-}
-
+// AccessTokenResponse representa la respuesta de un token de acceso.
 type AccessTokenResponse struct {
 	AccessToken string `json:"access_token"`
 	TokenType   string `json:"token_type"`
 	ExpiresIn   int    `json:"expires_in"`
 }
 
-func newSpotifyAuth(clientID, clientSecret string) *SpotifyAuth {
+// AuthClient define la interfaz para la autenticación con Spotify.
+type AuthClient interface {
+	GetAccessToken() (string, error)
+}
+
+// SpotifyAuth representa la autenticación de Spotify.
+type SpotifyAuth struct {
+	ClientID     string
+	ClientSecret string
+}
+
+// NewSpotifyAuth crea una nueva instancia de SpotifyAuth.
+func NewSpotifyAuth(clientID, clientSecret string) *SpotifyAuth {
 	return &SpotifyAuth{
 		ClientID:     clientID,
 		ClientSecret: clientSecret,
 	}
 }
-func (s *SpotifyAuth) getAccessToken() (string, error) {
+
+// GetAccessToken obtiene un token de acceso de Spotify.
+func (s *SpotifyAuth) GetAccessToken() (string, error) {
 	url := "https://accounts.spotify.com/api/token"
 	data := url2.Values{}
 	data.Set("grant_type", "client_credentials")
 
-	req, err := http.NewRequest("POST", url, bytes.NewBufferString(data.Encode()))
+	req, err := http.NewRequest("POST", url, strings.NewReader(data.Encode()))
 	if err != nil {
+		log.Printf("Error creando solicitud HTTP: %v", err)
 		return "", err
 	}
 
@@ -42,12 +54,14 @@ func (s *SpotifyAuth) getAccessToken() (string, error) {
 	client := &http.Client{Timeout: 10 * time.Second}
 	resp, err := client.Do(req)
 	if err != nil {
+		log.Printf("Error haciendo solicitud HTTP: %v", err)
 		return "", err
 	}
 	defer resp.Body.Close()
 
 	var tokenResp AccessTokenResponse
 	if err := json.NewDecoder(resp.Body).Decode(&tokenResp); err != nil {
+		log.Printf("Error decodificando respuesta: %v", err)
 		return "", err
 	}
 
