21c3c288228be67df6f7f8f365037911a75d3205
Thomas <viltetomas2003@gmail.com>
2024-04-22 17:10:57 -0300

    feat: Implementación de funciones de interacción con Discord

---
+++ internal/discord/message.go
@@ -0,0 +1,11 @@
+package discord
+
+import (
+	"github.com/Tomas-vilte/GoMusicBot/internal/discord/bot"
+	"github.com/bwmarrin/discordgo"
+)
+
+func GeneratePlayingSongEmbed(message *bot.PlayMessage) *discordgo.MessageEmbed {
+	// TODO: impl
+	return nil
+}
