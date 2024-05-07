48bd09f0ab02e298f9251cd3267c4f79e2021741
Thomas <viltetomas2003@gmail.com>
2024-05-06 21:54:14 -0300

    add access ssh

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -19,7 +19,7 @@ variable "ami_id" {
 variable "instance_type" {
   description = "Tipo de instancia EC2"
   type        = string
-  default     = "t3.micro"
+  default     = "t2.micro"
 }
 
 variable "key_name" {
