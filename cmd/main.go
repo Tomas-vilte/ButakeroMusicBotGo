ead2af077451b2d5cc1267a672162285dcaca6bd
Thomas <viltetomas2003@gmail.com>
2024-05-15 02:32:30 -0300

    feat: print

--- cmd/main.go
+++ cmd/main.go
@@ -25,7 +25,7 @@ var (
 )
 
 func main() {
-	fmt.Println("test")
+	fmt.Println("test drafter")
 	loggerCfg := zap.NewDevelopmentConfig()
 	loggerCfg.EncoderConfig.EncodeTime = zapcore.RFC3339TimeEncoder
 	logger, _ := loggerCfg.Build()
