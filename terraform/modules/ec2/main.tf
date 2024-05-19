4532bff3f7567cf7c52877733ea6fe2020f70389
Thomas <viltetomas2003@gmail.com>
2024-05-18 21:39:34 -0300

    modify infrastructure

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "instancia_ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
+  availability_zone = var.availability_zone
 
   subnet_id                   = var.subnet_id
   vpc_security_group_ids      = [var.security_group_id]
--- terraform/modules/subnet/main.tf
+++ terraform/modules/subnet/main.tf
@@ -1,6 +1,7 @@
 resource "aws_subnet" "subnet" {
   vpc_id     = var.vpc_id
   cidr_block = var.subnet_cidr_block
+  availability_zone = var.availability_zone
 
   tags = {
     Name = "subred-publica"
