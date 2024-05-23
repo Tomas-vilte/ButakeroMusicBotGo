98a6912434ef2227fe4e545299997917a4dc319b
Thomas <viltetomas2003@gmail.com>
2024-05-23 02:11:30 -0300

    add logger

--- lambdas/discord_notifications/internal/logging/logger.go
+++ lambdas/discord_notifications/internal/logging/logger.go
@@ -1,1 +1,35 @@
 package logging
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type Logger interface {
+	Info(msg string, fields ...zapcore.Field)
+	Error(msg string, fields ...zapcore.Field)
+}
+
+type ZapLogger struct {
+	logger *zap.Logger
+}
+
+func NewZapLogger() (*ZapLogger, error) {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		return nil, err
+	}
+	return &ZapLogger{logger: logger}, nil
+}
+
+func (l *ZapLogger) Close() error {
+	return l.logger.Sync()
+}
+
+func (l *ZapLogger) Info(msg string, fields ...zapcore.Field) {
+	l.logger.Info(msg, fields...)
+}
+
+func (l *ZapLogger) Error(msg string, fields ...zapcore.Field) {
+	l.logger.Error(msg, fields...)
+}
