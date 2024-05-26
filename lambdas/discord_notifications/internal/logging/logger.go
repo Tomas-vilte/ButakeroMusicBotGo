e4e513b830b4a262550c4b319cd50ab4793a57a7
Thomas <viltetomas2003@gmail.com>
2024-05-26 04:34:32 -0300

    docs: Agregar documentación a la interfaz Logger y la implementación ZapLogger

--- lambdas/discord_notifications/internal/logging/logger.go
+++ lambdas/discord_notifications/internal/logging/logger.go
@@ -5,15 +5,18 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
+// Logger define la interfaz para los métodos de registro de información y error.
 type Logger interface {
-	Info(msg string, fields ...zapcore.Field)
-	Error(msg string, fields ...zapcore.Field)
+	Info(msg string, fields ...zapcore.Field)  // Info registra un mensaje informativo.
+	Error(msg string, fields ...zapcore.Field) // Error registra un mensaje de error.
 }
 
+// ZapLogger es una implementación de la interfaz Logger utilizando Zap Logger.
 type ZapLogger struct {
 	logger *zap.Logger
 }
 
+// NewZapLogger crea una nueva instancia de ZapLogger.
 func NewZapLogger() (*ZapLogger, error) {
 	logger, err := zap.NewProduction()
 	if err != nil {
@@ -22,6 +25,7 @@ func NewZapLogger() (*ZapLogger, error) {
 	return &ZapLogger{logger: logger}, nil
 }
 
+// Close cierra el logger.
 func (l *ZapLogger) Close() error {
 	err := l.logger.Sync()
 	if err != nil && err.Error() != "sync /dev/stderr: invalid argument" {
@@ -30,10 +34,12 @@ func (l *ZapLogger) Close() error {
 	return nil
 }
 
+// Info registra un mensaje informativo.
 func (l *ZapLogger) Info(msg string, fields ...zapcore.Field) {
 	l.logger.Info(msg, fields...)
 }
 
+// Error registra un mensaje de error.
 func (l *ZapLogger) Error(msg string, fields ...zapcore.Field) {
 	l.logger.Error(msg, fields...)
 }
