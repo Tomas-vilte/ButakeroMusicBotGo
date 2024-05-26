617cbdc28422648e80f5128af9a758bb6d9b5d86
Thomas <viltetomas2003@gmail.com>
2024-05-26 04:36:14 -0300

    docs: Agregar documentación a la interfaz DiscordSession y la implementación DiscordSessionImpl

--- lambdas/discord_notifications/internal/messaging/discord_session.go
+++ lambdas/discord_notifications/internal/messaging/discord_session.go
@@ -4,12 +4,13 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+// DiscordSession define la interfaz para la sesión de Discord.
 type DiscordSession interface {
-	ChannelMessageSendEmbed(channelID string, embed *discordgo.MessageEmbed, options ...discordgo.RequestOption) (*discordgo.Message, error)
-	UserGuilds(limit int, beforeID, afterID string, withCounts bool, options ...discordgo.RequestOption) (st []*discordgo.UserGuild, err error)
-	GuildChannels(guildID string, options ...discordgo.RequestOption) (st []*discordgo.Channel, err error)
-	GuildChannelCreate(guildID, name string, ctype discordgo.ChannelType, options ...discordgo.RequestOption) (st *discordgo.Channel, err error)
-	GuildChannelCreateComplex(guildID string, data discordgo.GuildChannelCreateData, options ...discordgo.RequestOption) (st *discordgo.Channel, err error)
+	ChannelMessageSendEmbed(channelID string, embed *discordgo.MessageEmbed, options ...discordgo.RequestOption) (*discordgo.Message, error)                // ChannelMessageSendEmbed envía un mensaje con un embed al canal especificado.
+	UserGuilds(limit int, beforeID, afterID string, withCounts bool, options ...discordgo.RequestOption) (st []*discordgo.UserGuild, err error)             // UserGuilds obtiene los gremios a los que pertenece el usuario.
+	GuildChannels(guildID string, options ...discordgo.RequestOption) (st []*discordgo.Channel, err error)                                                  // GuildChannels obtiene los canales de un gremio especificado.
+	GuildChannelCreate(guildID, name string, ctype discordgo.ChannelType, options ...discordgo.RequestOption) (st *discordgo.Channel, err error)            // GuildChannelCreate crea un nuevo canal en un gremio.
+	GuildChannelCreateComplex(guildID string, data discordgo.GuildChannelCreateData, options ...discordgo.RequestOption) (st *discordgo.Channel, err error) // GuildChannelCreateComplex crea un nuevo canal en un gremio con opciones adicionales.
 }
 
 // DiscordSessionImpl es una implementación de la interfaz DiscordSession que se conecta a Discord utilizando discordgo.Session.
@@ -34,23 +35,27 @@ func NewDiscordSessionImpl(token string) (*DiscordSessionImpl, error) {
 	}, nil
 }
 
-// ChannelMessageSendEmbed Implementación de los métodos de la interfaz DiscordSession para DiscordSessionImpl.
+// ChannelMessageSendEmbed envía un mensaje con un embed al canal especificado.
 func (d *DiscordSessionImpl) ChannelMessageSendEmbed(channelID string, embed *discordgo.MessageEmbed, options ...discordgo.RequestOption) (*discordgo.Message, error) {
 	return d.session.ChannelMessageSendEmbed(channelID, embed, options...)
 }
 
+// UserGuilds obtiene los gremios a los que pertenece el usuario.
 func (d *DiscordSessionImpl) UserGuilds(limit int, beforeID, afterID string, withCounts bool, options ...discordgo.RequestOption) (st []*discordgo.UserGuild, err error) {
 	return d.session.UserGuilds(limit, beforeID, afterID, withCounts, options...)
 }
 
+// GuildChannels obtiene los canales de un gremio especificado.
 func (d *DiscordSessionImpl) GuildChannels(guildID string, options ...discordgo.RequestOption) (st []*discordgo.Channel, err error) {
 	return d.session.GuildChannels(guildID, options...)
 }
 
+// GuildChannelCreate crea un nuevo canal en un gremio.
 func (d *DiscordSessionImpl) GuildChannelCreate(guildID, name string, ctype discordgo.ChannelType, options ...discordgo.RequestOption) (st *discordgo.Channel, err error) {
 	return d.session.GuildChannelCreate(guildID, name, ctype, options...)
 }
 
+// GuildChannelCreateComplex crea un nuevo canal en un gremio con opciones adicionales.
 func (d *DiscordSessionImpl) GuildChannelCreateComplex(guildID string, data discordgo.GuildChannelCreateData, options ...discordgo.RequestOption) (st *discordgo.Channel, err error) {
 	return d.session.GuildChannelCreateComplex(guildID, data, options...)
 }
