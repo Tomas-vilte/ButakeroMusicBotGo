9690b88309fc3d30f49d1357352e3843c5aba0b6
thomas vilte <viltetomas2003@gmail.com>
2024-06-17 16:41:45 -0300

    refactor: introducir configuración personalizable para el caché Se agrega una estructura ConfigCaching para almacenar la configuración del caché, como el tamaño máximo, el tiempo de vida (TTL) y el intervalo de limpieza. Esto permite una mayor flexibilidad y facilita la personalización del caché según las necesidades.

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -1,6 +1,7 @@
 package cache
 
 import (
+	"container/list"
 	"github.com/Tomas-vilte/GoMusicBot/internal/discord/voice"
 	"github.com/Tomas-vilte/GoMusicBot/internal/logging"
 	"github.com/Tomas-vilte/GoMusicBot/internal/metrics"
@@ -9,18 +10,28 @@ import (
 	"time"
 )
 
-const cacheTTL = 5 * time.Minute
-const cleanupInterval = 1 * time.Minute
+type ConfigCaching struct {
+	MaxCacheSize    int
+	CacheTTL        time.Duration
+	CleanupInterval time.Duration
+}
+
+// DefaultCacheConfig contiene las configuraciones para el caché.
+var DefaultCacheConfig = ConfigCaching{
+	MaxCacheSize:    100,
+	CacheTTL:        10 * time.Minute,
+	CleanupInterval: 5 * time.Minute,
+}
 
 type (
-	// CacheEntry representa una entrada en el caché con datos de búsqueda y la marca de tiempo de la última actualización.
-	CacheEntry struct {
+	// Entry representa una entrada en el caché con datos de búsqueda y la marca de tiempo de la última actualización.
+	Entry struct {
 		Results     []*voice.Song
 		LastUpdated time.Time
 	}
 
-	// CacheManager define el comportamiento de un caché para almacenar y recuperar resultados de búsqueda.
-	CacheManager interface {
+	// Manager define el comportamiento de un caché para almacenar y recuperar resultados de búsqueda.
+	Manager interface {
 		Get(key string) []*voice.Song
 		Set(key string, results []*voice.Song)
 		DeleteExpiredEntries()
@@ -29,62 +40,134 @@ type (
 
 	// Cache es una implementación simple de caché en memoria.
 	Cache struct {
-		mu       sync.RWMutex
-		Lookup   map[string]CacheEntry
-		logger   logging.Logger
-		stopChan chan bool
-		metrics  metrics.CacheMetrics
+		mu         sync.RWMutex
+		Lookup     map[string]*list.Element
+		accessList ListInterface      // Interface ListInterface
+		entryPool  EntryPoolInterface // Interface EntryPoolInterface
+		logger     logging.Logger     // Interface logging.Logger
+		stopChan   chan bool
+		metrics    metrics.CacheMetrics // Interface metrics.CacheMetrics
+		timer      TimerInterface       // Interface TimerInterface
+		config     ConfigCaching        // Configurcacion ConfigCaching
 	}
 )
 
 // NewCache crea una nueva instancia de Cache.
-func NewCache(logger logging.Logger, metricsCache metrics.CacheMetrics) CacheManager {
+func NewCache(logger logging.Logger, metricsCache metrics.CacheMetrics, config ConfigCaching) Manager {
 	cache := &Cache{
-		Lookup:   make(map[string]CacheEntry),
-		logger:   logger,
-		stopChan: make(chan bool),
-		metrics:  metricsCache,
+		Lookup:     make(map[string]*list.Element),
+		accessList: newList(),
+		entryPool:  newEntryPool(),
+		logger:     logger,
+		stopChan:   make(chan bool),
+		metrics:    metricsCache,
+		timer:      newTimer(config.CleanupInterval),
+		config:     config,
 	}
 	go cache.cleanupExpiredEntries()
 	return cache
 }
 
 func (c *Cache) Get(key string) []*voice.Song {
+	start := time.Now() // Inicio del temporizador para medir la latencia
+
 	c.mu.RLock()
 	defer c.mu.RUnlock()
 
 	c.metrics.IncRequests()
 	c.metrics.IncGetOperations()
-	entry, ok := c.Lookup[key]
-	if !ok || time.Since(entry.LastUpdated) > cacheTTL {
-		c.logger.Info("Datos en caché expirados o no encontrados para la entrada", zap.String("input", key))
-		return nil
+	if element, ok := c.Lookup[key]; ok {
+		entry := element.Value.(*Entry)
+
+		c.accessList.MoveToFront(element)
+		if time.Since(entry.LastUpdated) > c.config.CacheTTL {
+			c.logger.Info("Datos en caché expirados para la entrada", zap.String("input", key))
+			c.accessList.Remove(element)
+			delete(c.Lookup, key)
+			c.entryPool.Put(entry)
+			c.metrics.IncEvictions()
+			c.metrics.SetCacheSize(float64(len(c.Lookup)))
+			c.metrics.IncLatencyGet(time.Since(start)) // Latencia de GET cuando la entrada está expirada
+			return nil
+		}
+		c.metrics.IncLatencyGet(time.Since(start)) // Latencia de GET cuando la entrada es válida
+		return entry.Results
 	}
-	c.logger.Info("Dato recuperado desde la cache: ", zap.String("Cancion", key))
-	return entry.Results
+	c.logger.Info("Datos en caché no encontrados para la entrada", zap.String("input", key))
+	c.metrics.IncLatencyGet(time.Since(start)) // Latencia de GET cuando la entrada no se encuentra
+	return nil
 }
 
 func (c *Cache) Set(key string, results []*voice.Song) {
+	start := time.Now() // Inicio del temporizador para medir la latencia
+
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	c.Lookup[key] = CacheEntry{
-		Results:     results,
-		LastUpdated: time.Now(),
+	if element, ok := c.Lookup[key]; ok {
+		entry := element.Value.(*Entry)
+		entry.Results = results
+		entry.LastUpdated = time.Now()
+		c.accessList.MoveToFront(element)
+		c.metrics.IncSetOperations()
+		c.metrics.SetCacheSize(float64(len(c.Lookup)))
+		c.metrics.IncLatencySet(time.Since(start)) // Latencia de SET cuando la entrada es actualizada
+		c.logger.Info("Datos actualizados en caché para la entrada", zap.String("input", key))
+		return
+	}
+
+	newEntry := c.entryPool.Get()
+	newEntry.Results = results
+	newEntry.LastUpdated = time.Now()
+	element := c.accessList.PushFront(newEntry)
+	c.Lookup[key] = element
+
+	if c.accessList.Len() > c.config.MaxCacheSize {
+		c.DeleteLRUEntry()
 	}
 	c.logger.Info("Datos almacenados en caché para la entrada", zap.String("input", key))
 	c.metrics.IncSetOperations()
 	c.metrics.SetCacheSize(float64(len(c.Lookup)))
+	c.metrics.IncLatencySet(time.Since(start)) // Latencia de SET cuando la entrada es nueva
 }
 
+func (c *Cache) DeleteLRUEntry() {
+	if c.accessList.Len() == 0 {
+		return
+	}
+
+	element := c.accessList.Back()
+	if element == nil {
+		return
+	}
+
+	deleteKey := ""
+	for key, e := range c.Lookup {
+		if e == element {
+			deleteKey = key
+			break
+		}
+	}
+	c.accessList.Remove(element)
+	delete(c.Lookup, deleteKey)
+	c.entryPool.Put(element.Value.(*Entry))
+	c.logger.Info("Entrada de caché LRU eliminada", zap.String("input", deleteKey))
+	c.metrics.IncEvictions()
+	c.metrics.SetCacheSize(float64(len(c.Lookup)))
+}
+
+// DeleteExpiredEntries esta función limpia entradas expiradas
 func (c *Cache) DeleteExpiredEntries() {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
 	now := time.Now()
-	for key, entry := range c.Lookup {
-		if now.Sub(entry.LastUpdated) > cacheTTL {
+	for key, element := range c.Lookup {
+		entry := element.Value.(*Entry)
+		if now.Sub(entry.LastUpdated) > c.config.CacheTTL {
+			c.accessList.Remove(element)
 			delete(c.Lookup, key)
+			c.entryPool.Put(entry)
 			c.logger.Info("Entrada de caché expirada eliminada", zap.String("input", key))
 			c.metrics.IncEvictions()
 		}
@@ -92,15 +175,19 @@ func (c *Cache) DeleteExpiredEntries() {
 	c.metrics.SetCacheSize(float64(len(c.Lookup)))
 }
 
+// Esta función limpia entradas expiradas usando un timer
 func (c *Cache) cleanupExpiredEntries() {
-	ticker := time.NewTicker(cleanupInterval)
-	defer ticker.Stop()
 	defer close(c.stopChan)
+	c.logger.Info("cleanupExpiredEntries iniciada")
+
 	for {
 		select {
-		case <-ticker.C:
+		case <-c.timer.C():
 			c.DeleteExpiredEntries()
+			c.timer.Reset(c.config.CleanupInterval)
+			c.logger.Info("cleanupExpiredEntries ejecutada")
 		case <-c.stopChan:
+			c.logger.Info("cleanupExpiredEntries detenida")
 			return
 		}
 	}
@@ -110,5 +197,7 @@ func (c *Cache) cleanupExpiredEntries() {
 func (c *Cache) Size() int {
 	c.mu.RLock()
 	defer c.mu.RUnlock()
-	return len(c.Lookup)
+	size := len(c.Lookup)
+	c.metrics.SetCacheSize(float64(size))
+	return size
 }
