8888e59db28032b7e625b10329d1bcb25f194f34
thomas vilte <viltetomas2003@gmail.com>
2024-07-14 18:53:01 -0300

    feat: Agregar struct Config para integración con AWS S3

---
+++ lambdas/process_audio/internal/config/config.go
@@ -0,0 +1,10 @@
+package config
+
+type Config struct {
+	BucketName      string
+	Region          string
+	AccessKey       string
+	SecretKey       string
+	Key             string
+	InputFileFromS3 string
+}
