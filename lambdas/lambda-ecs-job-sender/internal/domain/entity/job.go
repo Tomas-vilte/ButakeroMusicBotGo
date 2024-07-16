d4557f61908b1e959c665b68badbf1603a23fa27
thomas vilte <viltetomas2003@gmail.com>
2024-07-15 21:59:04 -0300

    entity: Definición de la Entidad Job

---
+++ lambdas/lambda-ecs-job-sender/internal/domain/entity/job.go
@@ -0,0 +1,19 @@
+package entity
+
+import "time"
+
+type Job struct {
+	ID                   string
+	S3Key                string
+	KEY                  string
+	Status               string
+	BucketName           string
+	Region               string
+	TaskDefinition       string
+	ClusterName          string
+	SecurityGroup        string
+	TaskRoleArn          string
+	TaskExecutionRoleArn string
+	CreatedAt            time.Time
+	UpdatedAt            time.Time
+}
