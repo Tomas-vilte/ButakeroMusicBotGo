6e47e7499a7a02bf10d5eefdcadac4cc37ba529f
thomas vilte <viltetomas2003@gmail.com>
2024-07-15 22:00:51 -0300

    tests: Implementación de Tests para el Repositorio de Jobs con DynamoDB

---
+++ lambdas/lambda-ecs-job-sender/internal/infrastructure/repository/mocks.go
@@ -0,0 +1,38 @@
+package repository
+
+import (
+	"context"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/stretchr/testify/mock"
+	"go.uber.org/zap"
+)
+
+type MockDynamoDBClient struct {
+	mock.Mock
+}
+
+func (m *MockDynamoDBClient) PutItem(ctx context.Context, params *dynamodb.PutItemInput, optFns ...func(*dynamodb.Options)) (*dynamodb.PutItemOutput, error) {
+	args := m.Called(ctx, params)
+	return args.Get(0).(*dynamodb.PutItemOutput), args.Error(1)
+}
+
+func (m *MockDynamoDBClient) Query(ctx context.Context, params *dynamodb.QueryInput, optFns ...func(*dynamodb.Options)) (*dynamodb.QueryOutput, error) {
+	args := m.Called(ctx, params)
+	return args.Get(0).(*dynamodb.QueryOutput), args.Error(1)
+}
+
+type MockLogger struct {
+	mock.Mock
+}
+
+func (m *MockLogger) Error(msg string, fields ...zap.Field) {
+	m.Called(msg, fields)
+}
+
+func (m *MockLogger) Info(msg string, fields ...zap.Field) {
+	m.Called(msg, fields)
+}
+
+func (m *MockLogger) With(fields ...zap.Field) {
+	m.Called(fields)
+}
