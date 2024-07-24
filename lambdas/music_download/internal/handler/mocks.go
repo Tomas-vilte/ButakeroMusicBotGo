4ec7707d66b168c52a51901948ed547509a2b0ac
thomas vilte <viltetomas2003@gmail.com>
2024-07-23 22:25:20 -0300

    tests: Agregados tests y mocks para el nuevo manejo de caché en el handler de eventos Lambda

--- lambdas/music_download/internal/handler/mocks.go
+++ lambdas/music_download/internal/handler/mocks.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"context"
+	"github.com/Tomas-vilte/GoMusicBot/lambdas/music_download/internal/types"
 	"github.com/stretchr/testify/mock"
 	"go.uber.org/zap/zapcore"
 	"io"
@@ -41,4 +42,35 @@ func (m *MockLogger) Error(msg string, fields ...zapcore.Field) {
 
 func (m *MockLogger) With(fields ...zapcore.Field) {
 	m.Called(fields)
+}
+
+type MockSongLooker struct {
+	mock.Mock
+}
+
+func (m *MockSongLooker) SearchYouTubeVideoID(ctx context.Context, query string) (string, error) {
+	args := m.Called(ctx, query)
+	return args.String(0), args.Error(1)
+}
+
+func (m *MockSongLooker) LookupSongs(ctx context.Context, videoID string) ([]*types.Song, error) {
+	args := m.Called(ctx, videoID)
+	return args.Get(0).([]*types.Song), args.Error(1)
+}
+
+type CacheMock struct {
+	mock.Mock
+}
+
+func (m *CacheMock) SetSong(ctx context.Context, key string, song *types.Song) error {
+	args := m.Called(ctx, key, song)
+	return args.Error(0)
+}
+
+func (m *CacheMock) GetSong(ctx context.Context, key string) (*types.Song, error) {
+	args := m.Called(ctx, key)
+	if args.Get(0) != nil {
+		return args.Get(0).(*types.Song), args.Error(1)
+	}
+	return nil, args.Error(1)
 }
