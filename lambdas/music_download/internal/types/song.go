5ec1a6da8b563dea2a132a0a60e77d1d442cc4bf
thomas vilte <viltetomas2003@gmail.com>
2024-07-29 22:40:55 -0300

    Agregar tipos Song y Channel para manejo de metadata musical

--- lambdas/music_download/internal/types/song.go
+++ lambdas/music_download/internal/types/song.go
@@ -3,10 +3,21 @@ package types
 import "time"
 
 type Song struct {
-	Type         string
-	Title        string
-	URL          string
-	Playable     bool
-	ThumbnailURL *string
-	Duration     time.Duration
+	Type         string        `json:"type"`
+	Title        string        `json:"title"`
+	URL          string        `json:"url"`
+	Playable     bool          `json:"playable"`
+	ThumbnailURL *string       `json:"thumbnail_url"`
+	Duration     time.Duration `json:"duration"`
+	Description  string        `json:"description"`
+	Category     string        `json:"category"`
+	Channel      Channel       `json:"channel"`
+	PublishedAt  time.Time     `json:"published_at"`
+	Tags         []string      `json:"tags"`
+}
+
+type Channel struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	URL  string `json:"url"`
 }
