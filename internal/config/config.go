ff5aca94f814758fcc6278f6c29f8ae927fd33ed
thomas vilte <viltetomas2003@gmail.com>
2024-08-09 01:02:39 -0300

    Eliminar implementación de almacenamiento en disco para canciones

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,13 +1,9 @@
 package config
 
 import (
-	"github.com/Tomas-vilte/GoMusicBot/internal/logging"
-	"os"
-	"path/filepath"
-
 	"github.com/Tomas-vilte/GoMusicBot/internal/discord/bot/store"
-	"github.com/Tomas-vilte/GoMusicBot/internal/discord/bot/store/file_storage"
 	"github.com/Tomas-vilte/GoMusicBot/internal/discord/bot/store/inmemory_storage"
+	"github.com/Tomas-vilte/GoMusicBot/internal/logging"
 )
 
 type Config struct {
@@ -31,21 +27,10 @@ type FileStoreConfig struct {
 	Dir string
 }
 
-func GetPlaylistStore(cfg *Config, guildID string, logger logging.Logger, persistent file_storage.StatePersistent) (store.SongStorage, store.StateStorage) {
+func GetPlaylistStore(cfg *Config, guildID string, logger logging.Logger) (store.SongStorage, store.StateStorage) {
 	switch cfg.Store.Type {
 	case "memory":
 		return inmemory_storage.NewInmemorySongStorage(logger), inmemory_storage.NewInmemoryStateStorage(logger)
-	case "file":
-		if err := os.MkdirAll(cfg.Store.File.Dir, 0755); err != nil {
-			panic(err)
-		}
-		path := filepath.Join(cfg.Store.File.Dir, guildID+".json")
-		songStore, err := file_storage.NewFileSongStorage(path, logger, persistent)
-		if err != nil {
-			panic(err)
-		}
-		stateStore := inmemory_storage.NewInmemoryStateStorage(logger)
-		return songStore, stateStore
 	default:
 		panic("tipo de store invalido")
 	}
