648c7b1d8c7fb9b685dfad615c8ac8ab43dd2a65
butakero <viltetomas2003@gmail.com>
2024-09-12 22:02:51 -0300

    Add Metadata struct for DynamoDB storage

---
+++ microservices/audio_processor/internal/domain/model/metadata.go
@@ -0,0 +1,16 @@
+package model
+
+type Metadata struct {
+	ID             string `json:"id"`              // Identificador único de la canción.
+	Title          string `json:"title"`           // Título de la canción.
+	Artist         string `json:"artist"`          // Artista de la canción.
+	Duration       int    `json:"duration"`        // Duración de la canción en segundos.
+	URLS3          string `json:"url_s3"`          // URL del archivo almacenado en S3.
+	URLYouTube     string `json:"url_youtube"`     // URL de la canción en YouTube.
+	DownloadDate   string `json:"download_date"`   // Fecha en que se descargó la canción.
+	Platform       string `json:"platform"`        // Plataforma de origen (e.g., YouTube).
+	ProcessingDate string `json:"processing_date"` // Fecha en que se procesó la canción.
+	Success        bool   `json:"success"`         // Indica si el procesamiento fue exitoso.
+	Attempts       int    `json:"attempts"`        // Número de intentos para descargar o procesar la canción.
+	Failures       int    `json:"failures"`        // Número de fallos durante el proceso.
+}
