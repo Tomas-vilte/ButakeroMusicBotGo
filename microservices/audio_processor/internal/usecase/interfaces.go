63ecc3d34a020c85f08803f24b188fe4d9713dae
butakero <viltetomas2003@gmail.com>
2024-09-27 16:48:49 -0300

    Add HTTP handlers for audio download initiation and status check

---
+++ microservices/audio_processor/internal/usecase/interfaces.go
@@ -0,0 +1,16 @@
+package usecase
+
+import (
+	"context"
+	"github.com/Tomas-vilte/ButakeroMusicBotGo/microservices/audio_processor/internal/domain/model"
+)
+
+type (
+	InitialDownloadUseCase interface {
+		Execute(ctx context.Context, song string) (string, error)
+	}
+
+	GetOperationStatusUseCase interface {
+		Execute(ctx context.Context, operationID, songID string) (model.OperationResult, error)
+	}
+)
