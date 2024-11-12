5e2af253106124cb943995617343c9ea0118e538
butakero <viltetomas2003@gmail.com>
2024-11-11 23:40:26 -0300

    Add project_name and environment variables

---
+++ microservices/audio_processor/song-downloader-infra/modules/database/variables.tf
@@ -0,0 +1,9 @@
+variable "project_name" {
+  description = "Nombre del proyecto"
+  type        = string
+}
+
+variable "environment" {
+  description = "Ambiente de despliegue"
+  type        = string
+}
