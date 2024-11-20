b1114f2672b333bbff43c6e8e0460a8bc5f32a78
butakero <viltetomas2003@gmail.com>
2024-11-20 18:20:31 -0300

    chore: configure AWS provider and S3 backend for Terraform state

---
+++ microservices/audio_processor/song-downloader-infra/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "song-download-tf-state"
+    key            = "prod/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
