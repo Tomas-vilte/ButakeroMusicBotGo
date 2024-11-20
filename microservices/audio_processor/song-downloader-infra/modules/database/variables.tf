137766252a1504f0c5a98243068bfb592d1adc43
butakero <viltetomas2003@gmail.com>
2024-11-20 18:08:29 -0300

    feat: add Application Load Balancer (ALB) for service routing

--- microservices/audio_processor/song-downloader-infra/modules/database/variables.tf
+++ microservices/audio_processor/song-downloader-infra/modules/database/variables.tf
@@ -7,3 +7,13 @@ variable "environment" {
   description = "Ambiente de despliegue"
   type        = string
 }
+
+variable "dynamodb_table_operations_tags" {
+  description = "Etiqueta para la tabla de operacion"
+  type = map(string)
+}
+
+variable "dynamodb_table_songs_tag" {
+  description = "Etiqueta para la tabla de canciones"
+  type = map(string)
+}
--- microservices/audio_processor/song-downloader-infra/modules/security_group_alb/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "security_group_alb_id" {
-  description = "ID del Security Group de ECS"
-  value       = aws_security_group.alb.id
-}
