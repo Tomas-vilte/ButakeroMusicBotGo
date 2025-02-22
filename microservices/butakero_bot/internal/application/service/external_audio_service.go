da286f7082a920e0fd640f63fdab4349b90677fd
butakero <viltetomas2003@gmail.com>
2025-02-22 16:11:45 -0300

    refactor: Rename audio service to external audio service

--- microservices/butakero_bot/internal/application/service/external_audio_service.go
+++ microservices/butakero_bot/internal/application/service/external_audio_service.go
@@ -9,28 +9,28 @@ import (
 	"go.uber.org/zap"
 )
 
-type audioService struct {
+type externalServiceAudio struct {
 	logger     logging.Logger
 	downloader ports.SongDownloader
 }
 
-func NewAudioService(downloader ports.SongDownloader, logger logging.Logger) ports.ExternalSongService {
-	return &audioService{
+func NewExternalAudioService(downloader ports.SongDownloader, logger logging.Logger) ports.ExternalSongService {
+	return &externalServiceAudio{
 		logger:     logger,
 		downloader: downloader,
 	}
 }
 
-func (s *audioService) RequestDownload(ctx context.Context, songName string) (*entity.DownloadResponse, error) {
-	response, err := s.downloader.DownloadSong(ctx, songName)
+func (e *externalServiceAudio) RequestDownload(ctx context.Context, songName string) (*entity.DownloadResponse, error) {
+	response, err := e.downloader.DownloadSong(ctx, songName)
 	if err != nil {
-		s.logger.Error("Error al solicitar la descarga",
+		e.logger.Error("Error al solicitar la descarga",
 			zap.String("songName", songName),
 			zap.Error(err))
 		return nil, fmt.Errorf("error al solicitar la descarga: %w", err)
 	}
 
-	s.logger.Info("Solicitud de descarga exitosa",
+	e.logger.Info("Solicitud de descarga exitosa",
 		zap.String("songName", songName),
 		zap.String("operationId", response.OperationID))
 
