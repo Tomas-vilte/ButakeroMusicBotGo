d8b6be92168b0a1bec22c177f945738254b14e33
butakero <viltetomas2003@gmail.com>
2025-02-25 20:18:04 -0300

    refactor: Update DownloadResponse status field to be optional; clean up unused code in embed generation

--- microservices/butakero_bot/internal/domain/entity/download_response.go
+++ microservices/butakero_bot/internal/domain/entity/download_response.go
@@ -3,5 +3,5 @@ package entity
 type DownloadResponse struct {
 	OperationID string `json:"operation_id"`
 	SongID      string `json:"song_id"`
-	Status      string `json:"status"`
+	Status      string `json:"status,omitempty"`
 }
