a919f1c55ab9d88c91d18a2fb309796baa3ead95
Tomas vilte <viltetomas2003@gmail.com>
2025-04-04 20:18:43 -0300

    refactor: Use specific event types for Discord player events

--- microservices/butakero_bot/internal/infrastructure/discord/player/events.go
+++ microservices/butakero_bot/internal/infrastructure/discord/player/events.go
@@ -1,25 +1,39 @@
 package player
 
 type (
-
-	// PlayerEvent representa eventos que pueden ocurrir en el reproductor
-	PlayerEvent struct {
-		Type    string
-		Payload EventPayload
+	// PlayerEvent es la interfaz que todos los eventos del reproductor deben implementar
+	PlayerEvent interface {
+		isPlayerEvent()
+		Type() string
 	}
+)
 
-	// EventPayload contiene datos adicionales para los eventos
-	EventPayload struct {
+type (
+	PlayEvent struct {
 		TextChannelID  *string
 		VoiceChannelID *string
 	}
+
+	PauseEvent struct{}
+
+	ResumeEvent struct{}
+
+	StopEvent struct{}
+
+	SkipEvent struct{}
 )
 
-// Event types
-const (
-	EventPlay   = "play"
-	EventPause  = "pause"
-	EventResume = "resume"
-	EventStop   = "stop"
-	EventSkip   = "skip"
-)
+func (e PlayEvent) Type() string   { return "play" }
+func (e PlayEvent) isPlayerEvent() {}
+
+func (e PauseEvent) Type() string   { return "pause" }
+func (e PauseEvent) isPlayerEvent() {}
+
+func (e ResumeEvent) Type() string   { return "resume" }
+func (e ResumeEvent) isPlayerEvent() {}
+
+func (e StopEvent) Type() string   { return "stop" }
+func (e StopEvent) isPlayerEvent() {}
+
+func (e SkipEvent) Type() string   { return "skip" }
+func (e SkipEvent) isPlayerEvent() {}
